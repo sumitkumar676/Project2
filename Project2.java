c0f98e7ab8841164db9cb9c6c594785413efbdc2
sumitkumarzk91 <sumitkumarzk91.com>
2020-02-05 09:49:11 +0530

    V2

--- Project2.java
+++ Project2.java
@@ -2,7 +2,8 @@ package com.ticgroup;
 
 public class Project2 {
 	public static void main(String[] args) {
-		System.out.println("ABC");
+		System.out.println("ABC122");
+		System.out.println("H");
 	}
 	}
 
